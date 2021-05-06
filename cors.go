f1706097d343dac7eee6acc169acb9ebf9ae447a
antklim <antklim@gmail.com>
2021-05-06 22:25:59 +1000

    CORS config validation refactored

--- cors.go
+++ cors.go
@@ -6,13 +6,30 @@ import (
 	"strings"
 )
 
-func Routes(paths []string, rules string) (http.Handler, error) {
+type rules struct {
+	raw string
+}
+
+func newRules(config string) rules {
+	return rules{raw: config}
+}
+
+func (r rules) Validate() error {
+	if strings.TrimSpace(r.raw) == "" {
+		return errors.New("invalid cors rules: cannot be empty")
+	}
+	return nil
+}
+
+func Routes(paths []string, config string) (http.Handler, error) {
 	if len(paths) == 0 {
 		return nil, errors.New("invalid paths list: cannot be empty")
 	}
 
-	if strings.TrimSpace(rules) == "" {
-		return nil, errors.New("invalid cors rules: cannot be empty")
+	r := newRules(config)
+
+	if err := r.Validate(); err != nil {
+		return nil, err
 	}
 
 	return nil, nil
