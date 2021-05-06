c9140a2c98dde8fbb816df21984d4dba54ceb9bf
antklim <antklim@gmail.com>
2021-05-07 00:17:17 +1000

    Rule parser implemented

--- rule.go
+++ rule.go
@@ -2,25 +2,47 @@ package cors
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
 	"strings"
 )
 
-// const (
-// 	rulesDlm  = '\n'
-// 	fieldsDlm = ';'
-// )
+const (
+	rulesDlm  string = "\n"
+	fieldsDlm string = ";"
+	fields    int    = 4
 
-// type rule struct {
-// 	raw string
-// 	p   []string // paths
-// 	o   []string // origins
-// 	h   []string // headers
-// 	m   []string // methods
-// }
+	wildcard string = "*"
+)
+
+const (
+	pIdx = iota
+	oIdx
+	hIdx
+	mIdx
+)
+
+// Methods excluding CONNECT, OPTIONS, TRACE
+var allMethods = []string{
+	http.MethodDelete,
+	http.MethodGet,
+	http.MethodHead,
+	http.MethodPatch,
+	http.MethodPost,
+	http.MethodPut,
+}
+
+type rule struct {
+	raw string
+	p   []string // paths
+	o   []string // origins
+	h   []string // headers
+	m   []string // methods
+}
 
 type rules struct {
 	raw string
-	// r   []rule
+	r   []rule
 }
 
 func newRules(config string) *rules {
@@ -31,5 +53,33 @@ func (r *rules) Parse() error {
 	if strings.TrimSpace(r.raw) == "" {
 		return errors.New("invalid cors rules: cannot be empty")
 	}
+
+	rawRules := strings.Split(r.raw, rulesDlm)
+	r.r = make([]rule, len(rawRules))
+	for i, rr := range rawRules {
+		pohm := strings.Split(rr, fieldsDlm)
+
+		if s := len(pohm); s != fields {
+			return fmt.Errorf("invalid cors rules: invalid amount of fields in rule %d, got %d want %d", i+1, s, fields)
+		}
+
+		p := []string{pohm[pIdx]} // paths
+		o := []string{pohm[oIdx]} // origins
+		h := []string{pohm[hIdx]} // headers
+		m := []string{pohm[mIdx]} // methods
+
+		if pohm[mIdx] == wildcard {
+			m = allMethods
+		}
+
+		r.r[i] = rule{
+			raw: rr,
+			p:   p,
+			o:   o,
+			h:   h,
+			m:   m,
+		}
+	}
+
 	return nil
 }
--- rule_internal_test.go
+++ rule_internal_test.go
@@ -1,9 +1,11 @@
 package cors
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRuleParseError(t *testing.T) {
@@ -22,6 +24,40 @@ func TestRuleParseError(t *testing.T) {
 			rules := newRules(tC.config)
 			err := rules.Parse()
 			assert.EqualError(t, err, tC.err)
+		})
+	}
+}
+
+func TestRuleParse(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		config string
+		r      *rules
+	}{
+		{
+			desc:   "parses wildcard config",
+			config: "*;*;;*",
+			r: &rules{
+				raw: "*;*;;*",
+				r: []rule{{
+					raw: "*;*;;*",
+					p:   []string{"*"},
+					o:   []string{"*"},
+					h:   []string{""},
+					m: []string{
+						http.MethodDelete, http.MethodGet, http.MethodHead,
+						http.MethodPatch, http.MethodPost, http.MethodPut,
+					},
+				}},
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			rules := newRules(tC.config)
+			err := rules.Parse()
+			require.NoError(t, err)
+			assert.Equal(t, tC.r, rules)
 		})
 	}
 }
